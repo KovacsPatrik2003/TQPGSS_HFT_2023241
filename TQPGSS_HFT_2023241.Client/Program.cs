48118e4c394d3380bc24afc4ff03761eaea79f87
kovacs.patrik <patrik.kovacs2003@gmail.com>
2023-10-21 12:35:52 +0200

    üres solution létrehozása

--- TQPGSS_HFT_2023241.Client/Program.cs
+++ TQPGSS_HFT_2023241.Client/Program.cs
@@ -7,6 +7,7 @@ namespace TQPGSS_HFT_2023241.Client
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
         }
     }
 }
---
+++ TQPGSS_HFT_2023241.Endpoint/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TQPGSS_HFT_2023241.Endpoint
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
