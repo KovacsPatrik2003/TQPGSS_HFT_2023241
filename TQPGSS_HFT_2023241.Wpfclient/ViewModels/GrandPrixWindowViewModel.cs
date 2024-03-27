17060e7c0267be9480b316c3a35d30e999724852
kovacs.patrik <patrik.kovacs2003@gmail.com>
2024-03-27 18:39:20 +0100

    grandprix and team window create and delete created

--- TQPGSS_HFT_2023241.Wpfclient/ViewModels/GrandPrixWindowViewModel.cs
+++ TQPGSS_HFT_2023241.Wpfclient/ViewModels/GrandPrixWindowViewModel.cs
@@ -1,12 +1,52 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using MovieDbApp.Wpfclient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TQPGSS_HFT_2023241.Models;
 
 namespace TQPGSS_HFT_2023241.Wpfclient.ViewModels
 {
-    public class GrandPrixWindowViewModel
+    public class GrandPrixWindowViewModel : ObservableRecipient
     {
+        public RestCollection<GrandPrix> GrandPrixs { get; set; }
+
+        public ICommand CreateGrandPrixCommand { get; set; }
+        public ICommand DeleteGrandPrixCommand { get; set; }
+        public ICommand UpdateGrandPrixCommand { get; set; }
+
+        private GrandPrix selectedGrandPrix;
+
+        public GrandPrix SelectedGrandPrix
+        {
+            get { return selectedGrandPrix; }
+            set
+            {
+                SetProperty(ref selectedGrandPrix, value);
+                (DeleteGrandPrixCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+        public GrandPrixWindowViewModel()
+        {
+            GrandPrixs = new RestCollection<GrandPrix>("http://localhost:18928/", "grandprix");
+
+            CreateGrandPrixCommand = new RelayCommand(() =>
+            {
+                GrandPrixs.Add(new GrandPrix()
+                {
+                    Name = "Ajak",
+                    Date = "Jan 1",
+                    WhoWon = 1
+                });
+            });
+
+            DeleteGrandPrixCommand = new RelayCommand(() => GrandPrixs.Delete(SelectedGrandPrix.Id),() => { return selectedGrandPrix != null; });
+
+        }
+
     }
 }
--- TQPGSS_HFT_2023241.Wpfclient/ViewModels/TeamWindowViewModel.cs
+++ TQPGSS_HFT_2023241.Wpfclient/ViewModels/TeamWindowViewModel.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using MovieDbApp.Wpfclient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using TQPGSS_HFT_2023241.Models;
 
 namespace TQPGSS_HFT_2023241.Wpfclient.ViewModels
 {
-    public class TeamWindowViewModel
+    public class TeamWindowViewModel : ObservableRecipient
     {
-    }
+        public RestCollection<Team> Teams { get; set; }
+
+		private Team selectedTeam;
+
+        public ICommand CreateTeamCommand { get; set; }
+        public ICommand DeleteTeamCommand { get; set; }
+        public ICommand UpdateTeamCommand { get; set; }
+
+        public Team SelectedTeam
+		{
+			get { return selectedTeam; }
+			set
+			{
+				SetProperty(ref selectedTeam, value);
+				(DeleteTeamCommand as RelayCommand).NotifyCanExecuteChanged();
+			}
+		}
+		public TeamWindowViewModel()
+		{
+			Teams = new RestCollection<Team>("http://localhost:18928/", "team");
+
+			CreateTeamCommand = new RelayCommand(() => Teams.Add(new Team()
+			{
+				Name = "Suzuki",
+				Points = 0
+			}));
+			DeleteTeamCommand = new RelayCommand(() => Teams.Delete(selectedTeam.Id), () => { return selectedTeam != null; });
+
+		}
+
+	}
 }
