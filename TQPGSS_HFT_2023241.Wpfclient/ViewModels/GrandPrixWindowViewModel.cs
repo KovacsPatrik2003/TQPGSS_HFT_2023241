48aa8bb22f9e4a2cfe89aee9fb38a7a972840f9a
kovacs.patrik <patrik.kovacs2003@gmail.com>
2024-04-09 14:07:20 +0200

    grand prix noncrud implementálva a wpf clientra

--- TQPGSS_HFT_2023241.Wpfclient/ViewModels/GrandPrixWindowViewModel.cs
+++ TQPGSS_HFT_2023241.Wpfclient/ViewModels/GrandPrixWindowViewModel.cs
@@ -3,6 +3,7 @@ using Microsoft.Toolkit.Mvvm.Input;
 using MovieDbApp.Wpfclient;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Numerics;
@@ -10,17 +11,38 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using System.Xml.Linq;
 using TQPGSS_HFT_2023241.Models;
 
 namespace TQPGSS_HFT_2023241.Wpfclient.ViewModels
 {
+    public class WinnerClass : ObservableObject
+    {
+        public string Name { get; set; }
+    }
+    public class CircuitClass : ObservableObject
+    {
+        //id name date winner
+        public int id { get; set; }
+        public string name { get; set; }
+        public string date { get; set; }
+        public string winner { get; set; }
+
+    }
+
     public class GrandPrixWindowViewModel : ObservableRecipient
     {
         public RestCollection<GrandPrix> GrandPrixs { get; set; }
+        public RestService NonCrud { get; set; }
+        public ObservableCollection<CircuitClass> CircuitNonCrud { get; set; }
+        public ObservableCollection<WinnerClass> WinnerNonCrud { get; set; }
 
         public ICommand CreateGrandPrixCommand { get; set; }
         public ICommand DeleteGrandPrixCommand { get; set; }
         public ICommand UpdateGrandPrixCommand { get; set; }
+        public ICommand VisszaCommand { get; set; }
+        public ICommand WinnerOfTheCircuitCommand { get; set; }
+        public ICommand CircuitDetalisCommand { get; set; }
 
         private GrandPrix selectedGrandPrix;
 
@@ -59,7 +81,9 @@ namespace TQPGSS_HFT_2023241.Wpfclient.ViewModels
             if (!IsInDesignMode)
             {
                 GrandPrixs = new RestCollection<GrandPrix>("http://localhost:18928/", "grandprix", "hub");
-
+                NonCrud = new RestService("http://localhost:18928/");
+                CircuitNonCrud = new ObservableCollection<CircuitClass>();
+                WinnerNonCrud = new ObservableCollection<WinnerClass>();
                 CreateGrandPrixCommand = new RelayCommand(() =>
                 {
                     
@@ -76,6 +100,42 @@ namespace TQPGSS_HFT_2023241.Wpfclient.ViewModels
                 UpdateGrandPrixCommand = new RelayCommand(() => GrandPrixs.Update(SelectedGrandPrix));
             }
             SelectedGrandPrix = new GrandPrix();
+            WinnerOfTheCircuitCommand = new RelayCommand(
+                () =>
+                {
+                    var a = NonCrud.Get<string>($"GrandPrixStat/WinnerOfTheCircuit/{SelectedGrandPrix.Name}");
+                    foreach (var item in a)
+                    {
+                        WinnerClass winner = new WinnerClass();
+                        winner.Name = item;
+                        WinnerNonCrud.Add(winner);
+                    }
+                }
+                );
+            CircuitDetalisCommand = new RelayCommand(
+                () =>
+                {
+                    var a = NonCrud.Get<CircuitClass>($"GrandPrixStat/GrandPrixDetails/{SelectedGrandPrix.Name}");
+                    foreach (var item in a)
+                    {
+                       
+                        CircuitNonCrud.Add(item);
+                    }
+                }
+                );
+            VisszaCommand = new RelayCommand(
+                () =>
+                {
+                    while (WinnerNonCrud.Count() != 0)
+                    {
+                        WinnerNonCrud.Remove(WinnerNonCrud[0]);
+                    }
+                    while (CircuitNonCrud.Count() != 0)
+                    {
+                        CircuitNonCrud.Remove(CircuitNonCrud[0]);
+                    }
+                }
+                );
         }
 
     }
